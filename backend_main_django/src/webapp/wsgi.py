82a3042f8c662b916b5f35da19b37711168a3204
Avakova Anastasiia <avakova.ast@gmail.com>
2025-08-05 14:13:52 +0300

    update pods

--- backend_main_django/src/webapp/wsgi.py
+++ backend_main_django/src/webapp/wsgi.py
@@ -8,6 +8,18 @@ https://docs.djangoproject.com/en/3.1/howto/deployment/wsgi/
 """
 
 import os
+import sys
+import traceback
+
+with open("/tmp/wsgi_log.txt", "w") as f:
+    try:
+        f.write("PYTHONPATH: {}\n".format(os.environ.get("PYTHONPATH")))
+        f.write("sys.path:\n" + "\n".join(sys.path) + "\n")
+        from django.core.wsgi import get_wsgi_application
+        application = get_wsgi_application()
+    except Exception:
+        f.write("ERROR:\n")
+        f.write(traceback.format_exc())
 
 from django.core.wsgi import get_wsgi_application
 
