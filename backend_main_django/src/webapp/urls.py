d78ec9045d285f760a047786c05781dd81697bb5
Avakova Anastasiia <avakova.ast@gmail.com>
2025-08-14 16:14:49 +0300

    add deployment

--- backend_main_django/src/webapp/urls.py
+++ backend_main_django/src/webapp/urls.py
@@ -1,9 +1,11 @@
 from django.shortcuts import redirect
 from django.contrib import admin
 from django.urls import path
+from django.http import HttpResponse
 
 
 urlpatterns = [
-    path('', lambda request: redirect('/admin/')),
-    path('admin/', admin.site.urls),
+    path("healthz/", lambda r: HttpResponse("ok")),
+    path("", lambda r: redirect("/admin/")),
+    path("admin/", admin.site.urls),
 ]
